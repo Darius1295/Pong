512a19b3ebb0b3a92b6595ea2677832e70fca18d
Darius1295 <headshrinker89@gmail.com>
2020-07-09 13:20:24 +0100

    Add sound effects

--- main.lua
+++ main.lua
@@ -23,8 +23,13 @@ function love.load()
 
     smallFont = love.graphics.newFont('font.ttf', 8)
     scoreFont = love.graphics.newFont('font.ttf', 32)
-
     love.graphics.setFont(smallFont)
+
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
 
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
@@ -99,28 +104,34 @@ function love.update(dt)
         	-- speed2 = ball.dx^2 + ball.dy^2
         	ball.dx = -ball.dx + 10
         	ball.dy = ((ball.y - (player1.y + player1.height/2)) / (player1.height/2)) * ball.dx
+        	sounds['paddle_hit']:play()
         end
         if ball:collides(player2) then
         	ball.x = ball.x - player1.width
         	-- speed2 = ball.dx^2 + ball.dy^2 
         	ball.dx = -ball.dx - 10
-        	ball.dy = ((ball.y - (player2.y + player2.height/2)) / (player2.height/2)) * -ball.dx   
+        	ball.dy = ((ball.y - (player2.y + player2.height/2)) / (player2.height/2)) * -ball.dx
+        	sounds['paddle_hit']:play()   
         end
 
         if ball.y < 0 then
         	ball.y = ball.y + ball.height
         	ball.dy = -ball.dy
+        	sounds['wall_hit']:play()
         end
         if ball.y + ball.height > VIRTUAL_HEIGHT then
         	ball.y = ball.y - ball.height
         	ball.dy = -ball.dy
+        	sounds['wall_hit']:play()
         end
 
         if ball.x + ball.width > VIRTUAL_WIDTH then
+        	sounds['score']:play()
         	player1Score = player1Score + 1
         	ball:reset(winningPlayer)
         end
         if ball.x < 0 then
+        	sounds['score']:play()
         	player2Score = player2Score + 1
         	ball:reset(winningPlayer)
         end
